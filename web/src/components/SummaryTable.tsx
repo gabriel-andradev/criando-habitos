c1675f06712269c39e0cc465684f48bdea65255b
Gabriel Andrade <gandrade.dev@gmail.com>
2023-02-07 21:42:47 -0300

    fix completed habits list

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -42,7 +42,7 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map(date => {
+        {summary.length > 0 && summaryDates.map(date => {
           const dayInSummmary = summary.find(day => {
             return dayjs(date).isSame(day.date, 'day')
           })
