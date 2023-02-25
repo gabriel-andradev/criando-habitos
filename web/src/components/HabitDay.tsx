edc94c65cb284a8fbc1d837a7569bb1620fb27f6
Gabriel Andrade <gandrade.dev@gmail.com>
2023-02-25 19:26:44 -0300

    update colors completed

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -26,18 +26,18 @@ export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDayPro
   return (
     <Popover.Root>
       <Popover.Trigger
-        className={clsx('w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background', {
+        className={clsx('w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded transition-colors focus:outline-none focus:ring-2 focus:ring-cyan-600 focus:ring-offset-2 focus:ring-offset-background', {
           'bg-zinc-900 border-zinc-800': completedPercentage === 0,
-          'bg-violet-900 border-violet-800': completedPercentage > 0 && completedPercentage <= 20,
-          'bg-violet-800 border-violet-700': completedPercentage > 20 && completedPercentage <= 40,
-          'bg-violet-700 border-violet-600': completedPercentage > 40 && completedPercentage <= 60,
-          'bg-violet-600 border-violet-500': completedPercentage > 60 && completedPercentage <= 80,
-          'bg-violet-500 border-violet-400': completedPercentage > 80,
+          'bg-cyan-900 border-cyan-800': completedPercentage > 0 && completedPercentage <= 20,
+          'bg-cyan-800 border-cyan-700': completedPercentage > 20 && completedPercentage <= 40,
+          'bg-cyan-700 border-cyan-600': completedPercentage > 40 && completedPercentage <= 60,
+          'bg-cyan-600 border-cyan-500': completedPercentage > 60 && completedPercentage <= 80,
+          'bg-cyan-500 border-cyan-400': completedPercentage > 80,
         })}
       />
 
       <Popover.Portal>
-        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background">
+        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col focus:outline-none focus:ring-2 focus:ring-cyan-600 focus:ring-offset-2 focus:ring-offset-background">
           <span className="font-semibold text-zinc-400">{dayOfWeek}</span>
           <span className="mt-1 font-extrabold leading-tight text-3xl">{dayAndMonth}</span>
 
