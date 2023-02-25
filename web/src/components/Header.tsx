5c76c09263f9cf0c4dbc75f584d66a41d5c66c79
Gabriel Andrade <gandrade.dev@gmail.com>
2023-02-25 19:28:33 -0300

    update color violet to cyan and size Dialog Close

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -3,10 +3,7 @@ import * as Dialog from '@radix-ui/react-dialog';
 import { Plus, X } from "phosphor-react";
 import { NewHabitForm } from './NewHabitForm';
 
-
-
 export default function Header() {
-
   return (
     <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
       <img src={logoImage} alt="Habits" />
@@ -15,17 +12,16 @@ export default function Header() {
 
         <Dialog.Trigger
           type='button'
-          className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background'
+          className='border border-cyan-500 font-semibold rounded px-6 py-4 flex items-center gap-3 hover:border-cyan-300 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-cyan-600 focus:ring-offset-2 focus:ring-offset-background'
         >
-          <Plus size={20} className='text-violet-500' />
-
+          <Plus size={22} className='text-cyan-400' />
           Novo hábito
         </Dialog.Trigger>
 
         <Dialog.Portal>
           <Dialog.Overlay className='w-screen h-screen bg-black/80 fixed inset-0' />
 
-          <Dialog.Content className='absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+          <Dialog.Content className='absolute p-10 bg-zinc-900 rounded-lg w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
 
             <Dialog.Close className='absolute right-6 top-6 rounded-lg text-zinc-400 hover:text-zinc-200 focus:outline-none'>
               <X size={24} aria-label="Fechar" />
