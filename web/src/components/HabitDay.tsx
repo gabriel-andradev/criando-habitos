25e14bcd79847991d7fcab5fe1b6d2ade21541f7
Gabriel Andrade <gandrade.dev@gmail.com>
2023-02-27 22:38:26 -0300

    add month in habits

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -18,6 +18,7 @@ export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDayPro
 
   const dayAndMonth = dayjs(date).format('DD/MM')
   const dayOfWeek = dayjs(date).format('dddd')
+  const month = dayjs(date).format('MMM')
 
   function handleCompletedChanged(completed: number) {
     setCompleted(completed)
@@ -26,15 +27,17 @@ export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDayPro
   return (
     <Popover.Root>
       <Popover.Trigger
-        className={clsx('w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded transition-colors focus:outline-none focus:ring-2 focus:ring-cyan-600 focus:ring-offset-2 focus:ring-offset-background', {
-          'bg-zinc-900 border-zinc-800': completedPercentage === 0,
+        className={clsx('w-10 h-10 bg-zinc-900 flex items-center text-zinc-900 text-opacity-70 justify-center border-2 border-zinc-800 rounded transition-colors focus:outline-none focus:ring-2 focus:ring-cyan-600 focus:ring-offset-2 focus:ring-offset-background', {
+          'bg-zinc-900 border-zinc-800 text-zinc-600': completedPercentage === 0,
           'bg-cyan-900 border-cyan-800': completedPercentage > 0 && completedPercentage <= 20,
           'bg-cyan-800 border-cyan-700': completedPercentage > 20 && completedPercentage <= 40,
           'bg-cyan-700 border-cyan-600': completedPercentage > 40 && completedPercentage <= 60,
           'bg-cyan-600 border-cyan-500': completedPercentage > 60 && completedPercentage <= 80,
-          'bg-cyan-500 border-cyan-400': completedPercentage > 80,
+          'bg-cyan-400 border-cyan-300': completedPercentage > 80,
         })}
-      />
+      >
+        {month}
+      </Popover.Trigger>
 
       <Popover.Portal>
         <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col focus:outline-none focus:ring-2 focus:ring-cyan-600 focus:ring-offset-2 focus:ring-offset-background">
